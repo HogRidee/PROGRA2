222e3f270b90dc5d86f12c726c260c6b510d309c
HogRidee <iaraoz061102@gmail.com>
2024-03-21 10:52:13 -0500

    Add class 01 files

---
+++ Laboratorios/01/2023-2/CrearBiblioteca_Parte1/Fuentes_Biblioteca_2023_2/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Ivan
+ *
+ * Created on 20 de marzo de 2024, 13:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
