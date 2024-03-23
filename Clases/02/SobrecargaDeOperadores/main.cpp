06c54912868443ef7452d3cdfd37b32c3325b8cc
HogRidee <iaraoz061102@gmail.com>
2024-03-23 13:10:07 -0500

    Finish tasks of class 02

--- Clases/02/SobrecargaDeOperadores/main.cpp
+++ Clases/02/SobrecargaDeOperadores/main.cpp
@@ -44,6 +44,13 @@ void operator++(Punto2D &p1){
     ++p1.y;
 }
 
+bool operator > (const Punto2D &p1, const Punto2D &p2){
+    if ((p1.x > p2.x) and (p1.y > p2.y)){
+        return true;
+    }
+    return false;
+}
+
 template <typename T>
 bool leerDatos(T* &arr, int &n, const char* filename){
     ifstream file(filename); //fstream para leer y escribir
@@ -58,9 +65,8 @@ bool leerDatos(T* &arr, int &n, const char* filename){
     
     while (true){
         file >> arr[n];
-        cout << arr[n] << endl;
-        if (file.eof()) break;
         ++n;
+        if (file.eof()) break;
     }
     
     file.close();
@@ -68,28 +74,36 @@ bool leerDatos(T* &arr, int &n, const char* filename){
     return true;
 }
 
+template <typename T>
+void mostrarDatos(T* arr, int n){
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << endl;
+    }
+}
+
 int main()
 {
     Punto2D p1{5,2};
     Punto2D p2{3,4};
     Punto2D p3 = p1 + p2;
-    cout<<p3<<endl;
-    p3 += p2;
+    cout<<p3<<endl; // 8,6
+    p3 += p2; // 11,10
     cout<<p3<<endl;
 
-     //TODO: implementar los operadores >, +=, ++
-//    if(p1 > p2)
-//        cout<<"P1 es mayor\n";
-//    else
-//        cout<<"P2 es mayor\n";    
-    p3 += p1;
-    cout<<p3<<endl; // 7, 7
+    //TODO: implementar los operadores >, +=, ++
+    if(p1 > p2)
+        cout<<"P1 es mayor\n";
+    else
+        cout<<"P2 es mayor\n";    
+    p3 += p1; // 16,12
+    cout<<p3<<endl; 
     ++p3;
-    cout<<p3<<endl; // 8, 8
+    cout<<p3<<endl; // 17,13
     
     Punto2D* arr = nullptr;
     int n = 0;
     leerDatos<Punto2D>(arr, n, "puntos2d.txt");
+    mostrarDatos<Punto2D>(arr, n);
     
     return 0;
 }
