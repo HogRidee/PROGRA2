7c9842b672cbcad4be30793da67dc0a33a4b1893
HogRidee <iaraoz061102@gmail.com>
2024-04-22 09:32:31 -0500

    Continue lab04 2024-1

--- Laboratorios/04/2023-2/PunterosGenericos-2023-2/PunterosGenericos.cpp
+++ Laboratorios/04/2023-2/PunterosGenericos-2023-2/PunterosGenericos.cpp
@@ -2,7 +2,17 @@
 #include "MuestraPunteros.h"
 
 void cargaproductos(void *&productos){
-    
+    ifstream file("productos2.csv");
+    if(not file.is_open()){
+        cout << "ERROR: NO SE PUDO ABRIR EL ARCHIVO productos2.csv" << endl;
+        exit(1);
+    }
+    int n = 0;
+    while(true){
+        if(file.eof()) break;
+        n++;
+    }
+    file.close();
 }
 
 void cargaclientes(void *&clientes){
